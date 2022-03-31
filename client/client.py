041a3bb7240b9edf68c7ba8be1dbaceb26ce60a5
dc-flo <flo.joh.rock@gmail.com>
2022-03-31 18:03:11 +0200

    modified graph

--- client/client.py
+++ client/client.py
@@ -11,10 +11,13 @@ from kivy.properties import ObjectProperty, NumericProperty
 import subprocess
 import os
 from kivy.config import Config
+from sympy import integrate
 from kivy.garden.graph import Graph, MeshLinePlot
-Config.set('graphics', 'window_state', 'maximized')
+from kivy.lang import Builder
+from kivymd.app import MDApp
+import scipy.integrate  
 
-ip = "http://192.168.4.1/"
+ip = "http://192.168.1.1/"
 ap_name = "ESP32-Access-Point"
 value_file = "data\\values.csv"
 raw_data_file = "data\\raw_data.csv"
@@ -25,6 +28,13 @@ rec_time = 5    #time in seconds for recording
 root = None
 graphlayout = None
 
+def short(a: np.array, l: int):
+    x = len(a)/l
+    arr = []
+    for i in range(l):
+        arr.append(np.mean(a[int(x*i):int(x*i+1)]))
+    return arr
+
 class MyAddButton(Widget):
     
     def addGraph(self):
@@ -34,7 +44,22 @@ class MyAddButton(Widget):
 
 
 class MyGraph(Widget):
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        data = pd.read_csv(value_file)
+        self.graph = self.children[0].children[1]
+        self.graph.xmax = np.max(data.time)
+        #self.graph.ymax = np.max(np.abs(data.posX))
+        self.plot = MeshLinePlot()
+        time = data.time
+        pos = data.barheightT1
+        self.plot.points = [(time[x], pos[x]) for x in range(len(time))]
+        print(self.plot.points)
+        self.graph.add_plot(self.plot)
+
+    def deleteSelf(self):
+        graphlayout.remove_widget(self)
 
 class MyGridLayout(Widget):
     
@@ -95,27 +120,39 @@ class MyGridLayout(Widget):
         values["rotY"] = round(values["rotY"] - values["rotY"][0], 2)
         values["rotZ"] = round(values["rotZ"] - values["rotZ"][0], 2)
         values["time"] = round(values["time"] - values["time"][0], 2)
+        values.drop(values.tail(1).index, inplace=True)
         values.to_csv(value_file, index=False)
         self.completeValues()
 
-    def integrate(self, vals:list) -> list:
-        ivals = []
-        for i in range(len(vals)):
-            ivals.append(round(vals[i]*tpr + np.sum(ivals[0:i]), 2))
-        return ivals
+    def barheight(self, p, T):
+        M = 0.02869
+        g = 9.807
+        R = 8.314
+        h = []
+        T = np.add(T, 273.15)
+
+        for i in range(len(p)):
+            if i == 0:
+                h.append(0)
+            else:
+                h.append(round(h[i-1] + (np.log(p[i]/p[i-1])/-((M*g)/(R*T[i]))), 2))
+        return h
+
 
     def completeValues(self):
         self.println('completing data')
         values = pd.read_csv(value_file)
-        values['velX'] = self.integrate(values['accX'])
-        values['velY'] = self.integrate(values['accY'])
-        values['velZ'] = self.integrate(values['accZ'])
-        values['posX'] = self.integrate(values['velX'])
-        values['posY'] = self.integrate(values['velY'])
-        values['posZ'] = self.integrate(values['velZ'])
-        values['rposX'] = self.integrate(values['rotX'])
-        values['rposY'] = self.integrate(values['rotY'])
-        values['rposZ'] = self.integrate(values['rotZ'])
+        values['velX'] = np.round(scipy.integrate.cumulative_trapezoid(values.accX, x=np.divide(values.time, 1000), initial=0), 2)
+        values['velY'] = np.round(scipy.integrate.cumulative_trapezoid(values.accY, x=np.divide(values.time, 1000), initial=0), 2)
+        values['velZ'] = np.round(scipy.integrate.cumulative_trapezoid(values.accZ, x=np.divide(values.time, 1000), initial=0), 2)
+        values['posX'] = np.round(scipy.integrate.cumulative_trapezoid(values.velX, x=np.divide(values.time, 1000), initial=0), 2)
+        values['posY'] = np.round(scipy.integrate.cumulative_trapezoid(values.velY, x=np.divide(values.time, 1000), initial=0), 2)
+        values['posZ'] = np.round(scipy.integrate.cumulative_trapezoid(values.velZ, x=np.divide(values.time, 1000), initial=0), 2)
+        values['rposX'] = np.round(scipy.integrate.cumulative_trapezoid(values.rotX, x=np.divide(values.time, 1000), initial=0), 2)
+        values['rposY'] = np.round(scipy.integrate.cumulative_trapezoid(values.rotY, x=np.divide(values.time, 1000), initial=0), 2)
+        values['rposZ'] = np.round(scipy.integrate.cumulative_trapezoid(values.rotZ, x=np.divide(values.time, 1000), initial=0), 2)
+        values['barheightT1'] = self.barheight(values.press, values.temp1)
+        values['barheightT2'] = self.barheight(values.press, values.temp2)
         values.drop(0)
         values.to_csv(value_file, index=False)
         self.println('finished data gathering')
@@ -148,6 +185,8 @@ class MyApp(App):
 
     def build(self):
         global root
+        Builder.load_file('layout.kv')
+        Config.set('graphics', 'window_state', 'maximized')
         root = MyGridLayout()
         return root
 
